d53c352f048adc4cfa31633f40da963186fe2c2a
veeresh-s_p <veereshsp1234@gmail.com>
2025-06-30 22:59:25 +0530

    completed assignment

---
+++ app.js
@@ -0,0 +1,13 @@
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+const express = require('express');
+const summarizeText = require('./summarizer');
+const app = express()
+app.use(express.json());
+app.post('/summarize', async (req, res) => {
+  const { text } = req.body;
+  const summary = await summarizeText(text);
+  res.json(summary);
+});
+app.listen(8080,()=>{
+    console.log("Server Running on port 8080")
+})
